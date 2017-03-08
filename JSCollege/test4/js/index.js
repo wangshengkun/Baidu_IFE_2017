da393653b4d313f7bf85ebae7447556c387be110
王圣坤 <619435969@qq.com>
2017-03-08 22:48:01 +0800

    修改

--- JSCollege/test4/js/index.js
+++ JSCollege/test4/js/index.js
@@ -1,11 +1,11 @@
 window.onload=function(){
-	// 获取文本内容
+	// 获取文本数字
 	function getNumber(){
 		var source = document.getElementsByTagName("input")[0].value;
 		if(source!=""){
 			var num = Number(source);
 			if(!isNaN(num)){
-			addSpan();
+				return num;
 			}else{
 			alert("输入文本须为数字。");
 			}
@@ -14,7 +14,7 @@ window.onload=function(){
 		}
 	}
 	// 小方块格式化
-	function addSpan(){
+	function addSpan(number){
 	var span = document.createElement("span");
 	span.style.display = "inline-block";
 	span.style.height = "40px";
@@ -23,18 +23,47 @@ window.onload=function(){
 	span.style.color = "#fff";
 	span.style.textAlign = "center";
 	span.style.marginLeft = "4px";
-	// span.text = num;
-	var wrap = document.getElementById("wrap");
-	wrap.appendChild(span);
+	span.style.fontSize = "12px";
+	span.style.lineHeight = "40px";
+	span.innerHTML = number;
+	// var wrap = document.getElementById("wrap");
+	// wrap.appendChild(span);
+	return span;
 	}
-
-	function end(){
-		getNumber();
+	//
+	function render(numData){
+		for(var i = 0,len = numData.length; i < len; i++){
+			var number = numData[i];
+			var span = addSpan(number);
+			var wrap = document.getElementById("wrap");
+			wrap.appendChild(span);
+		}
 	}
+	//左侧插入
+	var data = [];
+	function addLeft(){
+		var numberTxt = getNumber();
+		data.push(numberTxt);
+		render(data);
 
-	var btn = document.getElementsByTagName("button");
-	for(var i = 0, len = btn.length; i < len; i++){
-		btn[i].onclick = end;
+	}
+	function addRight(num){
+		data.unshift(num);
+	}
+	function removeLeft(num){
+		data.pop();
+	}
+	function removeRight(num){
+		data.shift();
 	}
 	
+	
+	function init(){
+		var btn = document.getElementsByTagName("button");
+		btn[0].onclick = addLeft;
+		btn[1].onclick = addRight;
+		btn[2].onclick = removeLeft;
+		btn[3].onclick = removeRight;
+	}
+	init();
 }
