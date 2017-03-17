197e6da2b3eedbd489e54ce5bc3a37cb11e67c39
王圣坤 <619435969@qq.com>
2017-03-17 20:12:23 +0800

    task9

--- JSCollege/test9/js/index.js
+++ JSCollege/test9/js/index.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 	var EventUtil = {
 		addHandler:function(element, type, handler){
-			if(element.addElementListener){
-				element.addElementListener(type, handler, false);
+			if(element.addEventListener){
+				element.addEventListener(type, handler, false);
 			}else if(element.attachEvent){
 				element.attachEvent("on" + type, handler);
 			}else{
@@ -15,10 +15,11 @@ window.onload = function(){
 	var btn = document.getElementsByTagName("button");
 	var dftBtn = btn[0];
 	var bftBtn = btn[1];
-	var dftSearch = btn[2];
-	var bftSearch = btn[3]
+	var addBtn = btn[2];
+	var deleteBtn = btn[3];
 	var order = [];
 	var timer = null;
+	var targetNode = null;
 
 	//深度优先遍历
 	function traverseDF(node){
@@ -51,6 +52,7 @@ window.onload = function(){
 				if(i < order.length){
 					order[i-1].style.backgroundColor = "#fff";	
 					if(order[i].firstChild.nodeValue.replace(/(^\s*)|(\s*$)/g, "") == txt){
+						
 						order[i].style.backgroundColor = "#00f";
 						clearInterval(timer);
 					}else{
@@ -67,6 +69,7 @@ window.onload = function(){
 		}
 	}
 
+	//初始化
 	function initialize(){
 		//每次调用广度优先遍历都要初始化index值，否则广度优先遍历只能执行一次，因为index是全局变量。
 		index = 0;
@@ -78,37 +81,85 @@ window.onload = function(){
 		}
 	}
 
+	//选中节点高亮显示
+	function highlight(){
+		var divList = document.getElementsByTagName("div");
+		for(var cur = 0;cur < divList.length; cur++){
+			EventUtil.addHandler(divList[cur], "click", function(event){
+				initialize();
+				this.style.backgroundColor = "#0f0";
+				event.stopPropagation();
+				targetNode = this;
+			});
+		}
+	}
+
+	//添加新节点
+	function addNode(txt){
+		var parent = this;
+		var newNode = document.createElement("div");
+		newNode.innerHTML = txt;
+		newNode.className = "newNode";
+		newNode.style.backgroundColor = "#fff";
+		parent.appendChild(newNode);
+	}
+
+	//删除节点
+	function deleteNode(){
+		var parent = this.parentNode;
+		parent.removeChild(this);
+		//更新targetNode,否则targetNode不存在，即null,使之父节点不存在，浏览器报错。
+		targetNode = null;
+	}
+
 	EventUtil.addHandler(dftBtn,"click", function(){
 		initialize();
 		traverseDF(root);
 		changeColor();
+		//为防止出现点击元素后直接遍历，最后再添加/删除节点的情况，
+		//targetNode必须在每次遍历后重置为null。
+		targetNode = null;
 	});
 
 	EventUtil.addHandler(bftBtn, "click", function(){
 		initialize();
 		traverseBF(root);
 		changeColor();
+		//同上，全局变量就是这点不好，但是目前没有想到合适的解决办法呀。
+		targetNode = null;
 	});
 
-	EventUtil.addHandler(dftSearch, "click", function(){
-		var txt = document.getElementsByTagName("input")[0].value;
-		initialize();
-		traverseDF(root);
-		if(txt != ""){
-			changeColor(txt);
+	//绑定添加节点函数
+	EventUtil.addHandler(addBtn, "click", function(event){
+		if(targetNode != null){
+			var txt = document.getElementsByTagName("input")[0].value;
+			if(txt !=""){
+				var txtArray = [].concat(txt);
+				addNode.apply(targetNode,txtArray);
+				highlight();
+				txt = "";
+			}else{
+				alert("请输入想要添加的内容。");
+			}
 		}else{
-			alert("请输入要查询的字符。");
+			alert("请先选中节点。");
 		}	
 	});
 
-	EventUtil.addHandler(bftSearch, "click", function(){
-		var txt = document.getElementsByTagName("input")[0].value;
-		initialize();
-		traverseBF(root);
-		if(txt != ""){
-			changeColor(txt);
+	//绑定删除节点函数
+	EventUtil.addHandler(deleteBtn, "click", function(){
+		if(targetNode != null){
+			var divList = document.getElementsByTagName("div");
+			if(divList.length >1){
+				deleteNode.call(targetNode);
+			}else{
+				alert("节点数目已为1，请勿删除。");
+			}
 		}else{
-			alert("请输入要查询的字符。");
-		}
+			alert("请先选中节点。");
+		}	
 	});
+
+	highlight();
+
 }
