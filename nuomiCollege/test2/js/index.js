6cf54f53aefa99645baa696c702d67feea6aef49
王圣坤 <619435969@qq.com>
2017-03-30 13:56:29 +0800

    task2

--- nuomiCollege/test2/js/index.js
+++ nuomiCollege/test2/js/index.js
@@ -13,53 +13,36 @@ var EventUtil = {
 		return event ? event : window.event;
 	},
 
-	getTarget: function(event){
-		return event.target || event.srcElement;
-	},
-
 	preventDefault: function(event){
 		if(event.preventDefault){
 			event.preventDefault();
 		}else{
 			event.returnValue = false;
 		}
-	},
-
-	stopPropagation: function(event){
-		if(event.stopPropagation){
-			event.stopPropagation();
-		}else{
-			event.cancleBubble = true;
-		}
 	}
+
 };
 
+//自定义菜单栏
 function customContextmenu(event){
 	event = EventUtil.getEvent(event);
 	EventUtil.preventDefault(event);
-	var flag =false;
+
 	//获取鼠标在页面中的横纵坐标
 	var mouseX = event.pageX,
 		mouseY = event.pageY;
-	// console.log(mouseX,mouseY);
 	
 	var wisdom = document.getElementsByTagName("p")[0];
-	//获取名言尺寸
+	//获取名言的宽高
 	var wisdomWidth = wisdom.offsetWidth,
 		wisdomHeight = wisdom.offsetHeight;
-	// console.log(wisdomWidth,wisdomHeight);
-	//获取名言的偏移量
-	var wisdomLeft = wisdom.offsetLeft,
-		wisdomTop = wisdom.offsetTop;
-	// console.log(wisdomLeft,wisdomTop);
 
 	var menu = document.getElementsByTagName("ul")[0];
 	menu.style.display = "block";
 	menu.style.position ="absolute";
-	//获取菜单栏的大小
+	//获取菜单栏的宽高
 	var menuWidth = menu.offsetWidth,
 		menuHeight = menu.offsetHeight;
-	// console.log("菜单框"+menuWidth,menuHeight);
 
 	//获取浏览器窗口大小
 	var pageWidth = window.innerWidth,
@@ -73,30 +56,31 @@ function customContextmenu(event){
 			pageHeight = document.body.clientHeight;
 		}
 	}
-	// console.log(pageWidth,pageHeight);
 
 	if(pageWidth >= (mouseX + menuWidth)){
 		menu.style.left = mouseX + "px";
 	}else{
 		// menu.style.cssText = "display:block;position:absolute;right:0px;";
-		menu.style.left = mouseX - menuWidth + "px"; 
+		menu.style.left = (mouseX - menuWidth) + "px"; 
 	}
 
 	if(pageHeight >= mouseY + menuHeight){
 		menu.style.top = mouseY + "px";
 	}else{
-		menu.style.bottom = pageHeight - mouseY + "px";
+		menu.style.top = (mouseY - menuHeight) + "px";
 	}
 
 	EventUtil.addHandler(menu, "click",function(){
 		menu.style.display = "none";
-	})
+	});
 
 	EventUtil.addHandler(document, "click", function(){
 		menu.style.display = "none";
-	})
+	});
 
 }
 
-var p = document.getElementsByTagName("p")[0];
-EventUtil.addHandler(p, "contextmenu", customContextmenu);
+var p = document.getElementsByTagName("p");
+for(var i = 0; i < p.length; i++){
+	EventUtil.addHandler(p[i], "contextmenu", customContextmenu);
+}
