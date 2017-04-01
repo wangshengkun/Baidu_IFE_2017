7e5b6d8c0e9ad472d7c1b8eac972a2b1b64519f8
王圣坤 <619435969@qq.com>
2017-04-01 22:26:49 +0800

    task4

---
+++ nuomiCollege/test4/js/index.js
@@ -0,0 +1,11 @@
+var EventUtil = {
+	addHandler:function(element, type, handler){
+		if(element.addEventListener){
+			element.addEventListener(type, handler, false);
+		}else if(element.attachEvent){
+			element.attachEvent("on" + type, handler);
+		}else{
+			element["on" + type] = handler;
+		}
+	}
+};
