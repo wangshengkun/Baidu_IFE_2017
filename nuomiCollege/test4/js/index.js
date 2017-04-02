f8737c1c9a7f89126ebd3a79bbefd56fdea761b0
王圣坤 <619435969@qq.com>
2017-04-02 23:09:08 +0800

    task4

--- nuomiCollege/test4/js/index.js
+++ nuomiCollege/test4/js/index.js
@@ -9,3 +9,27 @@ var EventUtil = {
 		}
 	}
 };
+
+var btn = document.getElementsByTagName("button");
+
+// var phoneNum = /^1(3\d|4[57]|5[^4]|7[01678]|8\d)\d{8}$/g;
+EventUtil.addHandler(btn[0], "click",function(){
+	// 手机号码类型：13[0-9], 14[5,7], 15[0, 1, 2, 3, 5, 6, 7, 8, 9], 17[0, 1, 6, 7, 8], 18[0-9]
+	var phoneNum = /^1(3\d|4[57]|5[^4]|7[01678]|8\d)\d{8}$/g;
+	var txt = document.getElementsByTagName("input")[0].value;
+	if(phoneNum.test(txt)){
+		alert("true");
+	}else{
+		alert("false");
+	}
+});
+
+EventUtil.addHandler(btn[1], "click", function(){
+	var string = /\b(\w+)\b\s+\1\b/g;
+	var txt = document.getElementsByTagName("input")[1].value;
+	if(string.test(txt)){
+		alert("true");
+	}else{
+		alert("false");
+	}
+});
